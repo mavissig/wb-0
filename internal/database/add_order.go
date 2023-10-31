7385dbe76729865f7713d7d52a29b4391d2ad4e7
Egor Kondratov <mavissig@student.21-school.ru>
2023-11-01 02:58:41 +0700

    fix: bd conflict

--- internal/database/add_order.go
+++ internal/database/add_order.go
@@ -32,6 +32,17 @@ INSERT INTO orders (
     oof_shard
 )
 VALUES (:order_uid, :track_number, :entry, :locale, :internal_signature, :customer_id, :delivery_service, :shardkey, :sm_id, :date_created, :oof_shard)
+ON CONFLICT (order_uid) DO UPDATE SET 
+	track_number = excluded.track_number,
+    entry = excluded.entry,
+    locale = excluded.locale,
+    internal_signature = excluded.internal_signature,
+    customer_id = excluded.customer_id,
+    delivery_service = excluded.delivery_service,
+    shardkey = excluded.shardkey,
+    sm_id = excluded.sm_id,
+    date_created = excluded.date_created,
+    oof_shard = excluded.oof_shard
 `
 	_, err = tx.NamedExecContext(context.Background(), query, order)
 	if err != nil {
@@ -40,17 +51,24 @@ VALUES (:order_uid, :track_number, :entry, :locale, :internal_signature, :custom
 
 	query = `
 INSERT INTO delivery (
-                      order_uid, 
-                      name,
-                      phone,
-                      zip,
-                      city,
-                      address,
-                      region,
-                      email
+	order_uid, 
+	name,
+	phone,
+	zip,
+	city,
+	address,
+	region,
+	email
 )
-
 VALUES (:order_uid,:name,:phone,:zip,:city,:address,:region,:email)
+ON CONFLICT (order_uid) DO UPDATE SET
+	name = excluded.name,
+	phone = excluded.phone,
+	zip = excluded.zip,
+	city = excluded.city,
+	address = excluded.address,
+	region = excluded.region,
+	email = excluded.email
 `
 	delivery := struct {
 		OrderUID string `db:"order_uid"`
@@ -67,19 +85,31 @@ VALUES (:order_uid,:name,:phone,:zip,:city,:address,:region,:email)
 
 	query = `
 INSERT INTO payment (
-                     order_uid,
-                     transaction,
-                     request_id,
-                     currency,
-                     provider,
-                     amount,
-                     payment_dt,
-                     bank,
-                     delivery_cost,
-                     goods_total,
-                     custom_fee
+	order_uid,
+	transaction,
+	request_id,
+	currency,
+	provider,
+	amount,
+	payment_dt,
+	bank,
+	delivery_cost,
+	goods_total,
+	custom_fee
 )
 VALUES (:order_uid, :transaction,:request_id,:currency,:provider,:amount,:payment_dt,:bank,:delivery_cost,:goods_total,:custom_fee)
+ON CONFLICT (transaction) DO UPDATE SET 
+	order_uid = excluded.order_uid,
+	transaction = excluded.transaction,
+	request_id = excluded.request_id,
+	currency = excluded.currency,
+	provider = excluded.provider,
+	amount = excluded.amount,
+	payment_dt = excluded.payment_dt,
+	bank = excluded.bank,
+	delivery_cost = excluded.delivery_cost,
+	goods_total = excluded.goods_total,
+	custom_fee = excluded.custom_fee
 `
 	payment := struct {
 		OrderUID string `db:"order_uid"`
@@ -137,6 +167,7 @@ ON CONFLICT (chrt_id) DO NOTHING
         chrt_id
     )
     VALUES (:order_uid, :chrt_id)
+    ON CONFLICT (order_uid, chrt_id) DO NOTHING
     `
 	for _, item := range order.Items {
 		orderItem := struct {
